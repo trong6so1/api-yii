a72e327279b45a8d05cb3d800c41aa3ad92fa6e3
trong6so1 <trong6so1@gmail.com>
2024-05-19 21:03:47 -0700

    Attach the callApi function to the controller to create order payment method

--- api/modules/v1/api/controllers/OrderPaymentMethodController.php
+++ api/modules/v1/api/controllers/OrderPaymentMethodController.php
@@ -2,40 +2,24 @@
 
 namespace api\modules\v1\api\controllers;
 
-use api\helper\response\ResultHelper;
-use yii\httpclient\Client;
 use Yii;
-use yii\httpclient\Exception;
 
 class OrderPaymentMethodController extends Controller
 {
-    /**
-     * @throws Exception
-     */
     public function actionCreate(): array
     {
+        $url = 'v1/order/order-payment-method/create';
+        $method = 'POST';
         $request = Yii::$app->request->queryParams;
-        $client = new Client([
-            'baseUrl' => Yii::$app->report->hostInfo,
-        ]);
         $header = [
             'Content-Type' => 'application/json',
             'Accept' => 'application/json',
             'Authorization' => $request['token'] ?? "",
         ];
-        $response = $client->post('v1/order/order-payment-method/create',
-            $request,
-            $header
-        )->send();
-        if ($response->isOk) {
-            return $response->data;
-        } else {
-            $response = json_decode($response->content);
-            $statusCode = $response->status;
-            $data = null;
-            $errol = $response->message;
-            $message = $response->name;
-        }
-        return ResultHelper::build($statusCode, $data, $errol, $message);
+        $config = [
+            'header' => $header,
+            'params' => $request,
+        ];
+        return Yii::$app->report->callApi($method, $url, $config);
     }
 }
